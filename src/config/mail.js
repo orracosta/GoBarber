00d6b09ae1a47f9f536a38e9f6e43e138c18852a
Rafael Costa <orracosta@icloud.com>
2021-02-15 19:44:32 -0300

    Added nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,10 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '82786237d3887b',
+    pass: '2d1251f667f8bf',
+  },
+  from: 'Equipe GoBarber <noreply@gobarber.com>',
+};
