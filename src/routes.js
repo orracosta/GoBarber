9a72ba404f1d800be49481d3d2a33b01e3a1decd
Rafael Costa <orracosta@icloud.com>
2021-02-15 19:05:31 -0300

    List/Check notification as read

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -21,10 +22,12 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.get('/schedule', ScheduleController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.get('/providers', ProviderController.index);
+routes.get('/notifications', NotificationController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
