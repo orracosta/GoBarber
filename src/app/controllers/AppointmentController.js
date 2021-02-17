7afbfb0fd47a5832b0da61c10bd4a87b7a9ef3a5
Rafael Costa <orracosta@icloud.com>
2021-02-17 18:04:53 -0300

    Checking if user is diferent of themself

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -53,6 +53,12 @@ class AppointmentController {
 
     const { provider_id, date } = req.body;
 
+    if (provider_id === req.userId) {
+      return res.status(401).json({
+        error: 'You can only create appointments with others providers',
+      });
+    }
+
     /**
      * Check if provider_id is a provider
      */
