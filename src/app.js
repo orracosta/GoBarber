f07763fd12be9cc14ec74f97eac9ee07d2e86891
Rafael Costa <orracosta@icloud.com>
2021-02-17 17:37:50 -0300

    Handling Exceptions

--- src/app.js
+++ src/app.js
@@ -1,19 +1,27 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
 
+import sentryConfig from './config/sentry';
 import routes from './routes';
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
-    this.middlewares();
+    Sentry.init(sentryConfig);
 
+    this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -23,6 +31,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  dsn:
+    'https://272f24ca39dc47c3afe0be525fed2bd6@o526313.ingest.sentry.io/5641688',
+};
